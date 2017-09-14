0ae8940eacdf75ce85b6db10a61ee67c0268b5b2
Howard Pinsley <hpinsley@gmail.com>
2017-09-14 19:50:58 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
+
+
