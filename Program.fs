e7166315526f07235671363ce0dd3652473cccdd
Howard Pinsley <hpinsley@gmail.com>
2017-09-15 06:22:08 -0400

    Multiple files

--- Program.fs
+++ Program.fs
@@ -1,25 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.org
+module Main
 
 open System
-
-let rec quicksort list =
-    match list with
-    | [] -> // If the list is empty
-                [] // return an empty list
-    | firstElem::otherElements -> // If the list is not empty
-        let smallerElements = // extract the smaller ones
-            otherElements
-            |> List.filter (fun e -> e < firstElem)
-            |> quicksort // and sort them
-
-        let largerElements = // extract the large ones
-            otherElements
-            |> List.filter (fun e -> e >= firstElem)
-            |> quicksort // and sort them
-        // Combine the 3 parts into a new list and return it
-
-        List.concat [smallerElements; [firstElem]; largerElements]
-//test
+open Sorting
 
 [<EntryPoint>]
 let main argv =
