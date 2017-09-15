bfe2468a1b9fdb5ed7d2ddeafb0b538ddde6b4e9
Howard Pinsley <hpinsley@gmail.com>
2017-09-15 06:03:39 -0400

    Show a quicksort implementation

--- Program.fs
+++ Program.fs
@@ -2,11 +2,32 @@
 
 open System
 
-let square x = x * x
+let rec quicksort list =
+    match list with
+    | [] -> // If the list is empty
+                [] // return an empty list
+    | firstElem::otherElements -> // If the list is not empty
+        let smallerElements = // extract the smaller ones
+            otherElements
+            |> List.filter (fun e -> e < firstElem)
+            |> quicksort // and sort them
+
+        let largerElements = // extract the large ones
+            otherElements
+            |> List.filter (fun e -> e >= firstElem)
+            |> quicksort // and sort them
+        // Combine the 3 parts into a new list and return it
+
+        List.concat [smallerElements; [firstElem]; largerElements]
+//test
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    printfn "%A" (quicksort [1;5;23;18;9;1;3])
+
     0 // return an integer exit code
+
+
 
 
