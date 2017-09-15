688ad2c8a3dd4c2033ab75d915ed02f8bad5834b
Howard Pinsley <hpinsley@gmail.com>
2017-09-15 06:28:21 -0400

    Sort both numbers and strings

--- Program.fs
+++ Program.fs
@@ -8,9 +8,6 @@ open Utils.Sorting
 let main argv =
     printfn "Hello World from F#!"
     printfn "%A" (quicksort [1;5;23;18;9;1;3])
+    printfn "%A" (quicksort ["dog";"cat";"elephant";"bat"])
 
     0 // return an integer exit code
-
-
-
-
